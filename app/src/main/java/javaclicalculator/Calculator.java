24e113341257e7f95c5337be666a172877413017
Kmuzaki <keihanmuzaki0480@gmail.com>
2025-08-26 15:37:24 +0800

    initMainMenu

--- app/src/main/java/javaclicalculator/Calculator.java
+++ app/src/main/java/javaclicalculator/Calculator.java
@@ -8,6 +8,7 @@ public class Calculator extends Operations {
     
     // Scanner for user input
     Scanner scanner = new Scanner(System.in);
+    String input;
 
     // Clear screen method
     public void clearScreen() {  
@@ -38,12 +39,59 @@ public class Calculator extends Operations {
         return a / b;
     }
 
-    // Getter and setter for result
-    public double getResult() {
-        return result;
+    public void mainMenu(){
+        
+        while (true){
+            System.out.println("=============================");
+            System.out.println("Please select your selection:");
+            System.out.println("1. Add new student");
+            System.out.println("2. View all students");
+            System.out.println("3. Edit student information");
+            System.out.println("4. Search student by ID");
+            System.out.println("5. Remove student");
+            System.out.println("0. Exit");
+            System.out.println("=============================");
+
+            try {
+                System.out.print("Type in selection> ");
+                input = scanner.nextLine();
+
+                clearScreen();
+                switch (input) {
+                    case 1:
+                        addStudent();
+                        break;
+                    case 2:
+                        gettAllStudents();
+                        break;
+                    case 3:
+                        updateStudent();
+                        break;
+                    case 4:
+                        // getStudentById();
+                        break;
+                    case 5:
+                        removeStudent();
+                        break;
+                    case 0:
+                        System.out.println("See you next time!");
+                        scanner.close();
+                        break;
+                    default:
+                        System.out.println("Invalid Input! Please check your input again.");
+                        System.out.println("");
+                        break;
+                }
+                if (input == "0") {
+                    break;
+                }
+            } catch (Exception e) {
+                System.out.println("");
+                // System.out.println("Invalid Input! Please check your input again.");
+                // clearScreen();
+                scanner.nextLine();
+            }
+        }
     }
 
-    public void setResult(double result) {
-        this.result = result;
-    }
 }
